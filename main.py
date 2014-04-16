5c16dffb16891be1895921b3e2d9d3cb010087ee
bornreddy <marisa.reddy@gmail.com>
2014-04-16 13:19:53 -0400

    finished triangle area function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2, cv
  
 # Define a on_mouse function. 
@@ -12,8 +13,7 @@ def on_mouse(event, x, y, flags, params):
       cv2.line(img, parenchyma[-2], parenchyma[-1], (255,0,0), 1)
       cv2.imshow('image', img)
 
-
-
+#test function - prints array of parenchyma parameter
 def print_par():
   for p in parenchyma:
     print p
@@ -23,19 +23,22 @@ img = cv2.imread('lt_long.jpg', 0)
 cv2.imshow('image', img)
 cv.SetMouseCallback('image', on_mouse, 0)
 
-# cv2.line(img, (0,0), (500,500), (255,0,0), 5)
-# cv2.imshow('image', img)
-# cv.SetMouseCallback('image', on_mouse, 0)
+def draw_p_area():
+  for x in range( 1, (len(parenchyma)-1) ):
+    points = np.array([parenchyma[0], parenchyma[x], parenchyma[x+1]])
+    cv2.polylines(img, np.int32([points]), 1, (255,255,255))
+    cv2.imshow('image',img)
 
-
- 
-# Wait until a key is pressed to destory all windows
 while(1):
+  # wait indefinitely for a key to be pressed
   k = cv2.waitKey(0)
+  # if key pressed is 'enter' connect first and last point, and calculate area.
   if (k == 13):
     cv2.line(img, parenchyma[0], parenchyma[-1], (255,0,0), 1)
     cv2.imshow('image', img)
-    print "got connectionss duuuude"
+    draw_p_area()
+  # if key pressed is 'esc' close window
   elif (k == 27):
     cv2.destroyAllWindows()
+
     break 
